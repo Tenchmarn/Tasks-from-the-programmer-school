6a62612b45a71b2012512c20a3a2c41c4e55ab7a
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-08-29 01:06:48 +0300

    Add files via upload

---
+++ (0002).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int Fun_ts(int e_1) {
+	int r = 0;
+	for (int i = 0; i <= e_1; i++) r += i;
+	return r;
+}
+void main() {
+	int e_1;
+	cin >> e_1;
+	cout << Fun_ts(e_1) << endl;
+}
