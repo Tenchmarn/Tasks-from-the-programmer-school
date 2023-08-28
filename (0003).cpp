86fe5efe062ef874bc1f33c1a612bcebc0265d8f
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-08-29 01:07:01 +0300

    Add files via upload

---
+++ (0003).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+void main() {
+	int e_1;
+	cin >> e_1;
+	if (e_1 == 5) {
+		cout << 25;
+	}
+	else {
+		e_1 /= 10;
+		e_1 *= e_1 + 1;
+		cout << e_1 << 25 << endl;
+	}
+}
