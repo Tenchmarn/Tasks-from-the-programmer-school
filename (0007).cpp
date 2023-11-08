17091b6657a29b6ef49d8d79b6658fe243f46160
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-08 17:13:57 +0300

    Add files via upload

---
+++ (0007).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int A_1;
+	int max_T=0;
+	//cin >> A_1 >> A_2 >> A_3;
+	for (int i = 1; i <= 3; i++) {
+		cin >> A_1;
+		if (A_1 > max_T) {
+			max_T = A_1;
+		}
+	}
+	cout << max_T;
+	return 0;
+}
