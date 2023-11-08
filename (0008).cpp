0e0e03374a94f654bcab9d368e2dbc5bd16d6b0b
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-11-09 00:47:23 +0300

    Add files via upload

---
+++ (0008).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a_1, a_2, a_3;
+	cin >> a_1 >> a_2 >> a_3;
+	if ((a_1 * a_2) == a_3) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+	return 0;
+}
