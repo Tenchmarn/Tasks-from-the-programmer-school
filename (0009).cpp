be0f5597cb7be521c7faa40286f660ccc440cdc4
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2023-12-18 16:24:54 +0300

    Add files via upload

---
+++ (0009).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int A_1;
+	cin >> A_1;
+	int *arr = new int[A_1];
+	int sum_Pos = 0, TP_0 = 1, max_Ind = 0, min_Ind = 0;
+	for (int i = 0; i < A_1; i++) {
+		cin >> arr[i];
+		if (arr[i] > 0) sum_Pos += arr[i];
+		if (arr[i] > arr[max_Ind]) max_Ind = i;
+		if (arr[i] < arr[min_Ind]) min_Ind = i;
+	}
+	for (int i = fmin(min_Ind, max_Ind); i < fmax(min_Ind, max_Ind); i++) {
+		TP_0 *= arr[i];
+	}
+	cout << sum_Pos << " " << TP_0 << endl;
+	return 0;
+}
