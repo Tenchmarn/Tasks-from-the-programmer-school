699c18c354c8874a7ee321bf81a84e10c4a249b1
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-01-07 23:06:26 +0300

    Add files via upload

---
+++ (0010).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	for (int x = -100; x <= 100; x++) {
+		if (a* x* x* x + b * x * x + c * x + d == 0) {
+			cout << x << " ";
+		}
+	}
+	return 0;
+}
