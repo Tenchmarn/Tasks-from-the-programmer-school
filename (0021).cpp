6ea2c9f49b4f5a077789131a617ff202275d3df5
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-02-04 16:24:58 +0300

    Add files via upload

---
+++ (0021).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int r1, r2, r3;
+	cin >> r1 >> r2 >> r3;
+	if ((r1 < r2) && (r1 < r3)) {
+		if (r2 < r3) {
+			cout << r3 - r1;
+		}
+		else {
+			cout << r2 - r1;
+		}
+	}
+	if ((r1 > r2) && (r1 > r3)) {
+		if (r2 > r3) {
+			cout << r1 - r3;
+		}
+		else {
+			cout << r1 - r2;
+		}
+	}
+
+	return 0;
+}
