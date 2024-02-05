fd4e1fd3cf1e906aedfef657437c0dff00afb74d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-02-05 16:03:11 +0300

    Add files via upload

---
+++ (0061).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a_1, a_2, SUM_a1, SUM_a2;
+	SUM_a1 = SUM_a2 = 0;
+	for (int i = 0; i < 4; i++) {
+		cin >> a_1 >> a_2;
+		SUM_a1 += a_1;
+		SUM_a2 += a_2;
+	}
+	if (SUM_a1 < SUM_a2) cout << "2";
+	if (SUM_a2 < SUM_a1) cout << "1";
+	if (SUM_a1 == SUM_a2) cout << "DRAW";
+	return 0;
+}
