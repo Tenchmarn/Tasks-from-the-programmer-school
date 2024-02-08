35641542e63ce517a4a3b50cbacc747567cbd2d8
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-02-08 17:02:14 +0300

    Add files via upload

---
+++ (0755).cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	a += b -= c;
+	if (a < (a + c) && (a > 0)) cout << a;
+	else cout << "Impossible";
+	return 0;
+}
