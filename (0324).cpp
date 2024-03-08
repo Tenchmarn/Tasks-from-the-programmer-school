232f21382cd2910829c521619bccf1cf05eee141
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-08 23:04:36 +0300

    Add files via upload

---
+++ (0324).cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	const int S_0 = 4;
+	char a1[S_0];
+	for (int i = 0; i < S_0; i++) cin >> a1[i];
+	if (a1[0] == a1[3] && a1[1] == a1[2]) cout << "YES";
+	else cout << "NO";
+	return 0;
+}
