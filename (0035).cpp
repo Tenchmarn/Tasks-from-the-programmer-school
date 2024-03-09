a60b202527336895a7be034bf41ddfd58ae10e92
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-09 14:36:41 +0300

    Add files via upload

---
+++ (0035).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int k, m, n;
+	cin >> k;
+	int* arr = new int[k];
+	for (int i = 0; i < k; i++) {
+		cin >> n >> m;
+		arr[i] = 19 * m + (n + 239) * (n + 366) / 2;
+	}
+	cout << endl;
+	for (int j = 0; j < k; j++) {
+		cout << arr[j] << endl;
+	}
+	return 0;
+}
