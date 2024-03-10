9a2bca1fb4fcffc01037b51e6aeb208e49f83329
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-10 15:03:01 +0300

    Add files via upload

---
+++ (0697).cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int L, W, H, S, A=16;
+	cin >> L >> W >> H;
+	S = 2 * H * (W + L);
+	if ((S % 16) == 0) { cout << S / 16; }
+	else cout << (S / 16) + 1;
+	return 0;
+}
