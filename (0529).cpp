203abc754ca0a7f0c69a62237e6952b2d5c42632
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-12 16:22:48 +0300

    Add files via upload

---
+++ (0529).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+double my_sqrt(double);
+int main() {
+	int x1, x2, y1, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	cout << my_sqrt(((x2 - x1) * (x2 - x1))+((y2 - y1) * (y2 - y1)));
+	return 0;
+}
+double my_sqrt(double x) {
+    if (x < 0) {
+        return NAN;
+    }
+    double left = 0, right = x + 1;
+    for (int i = 0; i < 1000; i++) {
+        double middle = (left + right) / 2;
+        if (middle * middle < x) {
+            left = middle;
+        }
+        else {
+            right = middle;
+        }
+    }
+
+    return left;
+}
