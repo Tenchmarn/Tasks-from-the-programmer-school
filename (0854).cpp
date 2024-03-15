b2669673313c75fb56ac3c005d73bf46673aecce
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-15 16:02:54 +0300

    Add files via upload

---
+++ (0854).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int a1, a2;
+	string amrt;
+	cin >> a1 >> a2;
+	cin >> amrt;
+	if(amrt == "freeze")cout << min(a1, a2) << endl;
+	if(amrt == "heat")cout << max(a1, a2) << endl;
+	if(amrt == "auto")cout << a2 << endl;
+	if(amrt == "fan")cout << a1 << endl;
+	return 0;
+}
