79cd4287ef6dc25d1c882b103c7b9af23f2a39f6
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-16 18:37:04 +0300

    Add files via upload

---
+++ (0052).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string a1;
+	int sum1=0, sum2=0;
+	cin >> a1;
+	for (int i = 0; i < 3; i++) {
+		sum1 += a1[i] - '0';
+	}
+	for (int j = 3; j < 6; j++) {
+		sum2 += a1[j] - '0';
+	}
+	if (sum1 == sum2) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
