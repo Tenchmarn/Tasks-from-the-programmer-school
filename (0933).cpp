a5b5bdb995fda0a0d569be56367027f1ef6a75d0
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-18 18:27:36 +0300

    Add files via upload

---
+++ (0933).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int A, B, C, T, sum;
+	cin >> A >> B >> C >> T;
+	if (A < T) {
+		sum = B * A;
+		T -= A;
+		sum += T * C;
+		cout << sum;
+	}
+	else cout << T * B;
+	return 0;
+}
