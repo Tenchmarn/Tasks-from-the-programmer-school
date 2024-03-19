d6fbf50832aeae00b4fdd8ddfd72525c5b0650e5
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-19 18:44:43 +0300

    Add files via upload

---
+++ (0606).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int X, Y, Z;
+	cin >> X >> Y >> Z;
+	if (X <= Y + Z && Y <= X + Z && Z <= Y + X) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+	return 0;
+}
