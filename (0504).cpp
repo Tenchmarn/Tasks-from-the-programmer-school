0fad617e5cee5c4f57098bd6b8e3f4e555922d4d
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-03-23 15:04:30 +0300

    Add files via upload

---
+++ (0504).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	char L_1 = 'V', L_2 = 'G', L_3 = 'C';
+	a %= 3;
+	if (a == 0) cout << "GCV";
+	if (a == 1) cout << "VGC";
+	if (a == 2) cout << "CVG";
+	return 0;
+}
