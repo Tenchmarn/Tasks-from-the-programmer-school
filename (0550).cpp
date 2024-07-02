e1c574926daf9864ad430b8d8ac9853403381fd1
Tenchmarn <107813409+Tenchmarn@users.noreply.github.com>
2024-07-02 17:09:18 +0300

    Add files via upload

---
+++ (0550).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int year;
+	cin >> year;
+	if ((year % 400 == 0) || ((year % 4 == 0) && (year%100!=0))) {
+		cout << "12/09/";
+	} else {
+		cout << "13/09/";
+	}
+	if (year < 10) cout << 0;
+	if (year < 100) cout << 0;
+	if (year < 1000) cout << 0;
+	cout << year;
+	return 0;
+}
